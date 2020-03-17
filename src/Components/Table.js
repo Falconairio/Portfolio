de9726531ed1d560f02c09f8d0e41078a6fb50d2
Griffith <2ndgriff@gmail.com>
2020-03-17 18:09:05 +0000

    updated Table component, might not keep as I dont know ifI like the vibe

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -4,11 +4,18 @@ export default class Table extends Component {
     render() {
         return (
             <div className = "table-container">
-                <div className = "triangle-spacer">
-                    <div className = "triangle" />
-                    <div className = "triangle2" />
+                <div className = "table-top-line" />
+                <div className = "table-base-container" >
+                    <div className = "triangle-spacer">
+                        <div className = "triangle" />
+                        <div className = "triangle2" />
+                    </div>
+                    <div className = "table-line" />
                 </div>
-                <div className = "table-line" />
+                <div className = "table-legs">
+                    <div className = "table-leg" id = "leg1" />
+                    <div className = "table-leg"/>
+                </div>
             </div>
         )
     }
